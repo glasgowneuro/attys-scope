d6fd9d7b097aaa335ac732b829185e77a27e1f37
Bernd Porr <mail@berndporr.me.uk>
2016-11-02 08:43:34 +0000

    Added new class which communicates with the Attys That should work both with Linux and Win

---
+++ AttysComm.cpp
@@ -0,0 +1,271 @@
+#include "AttysComm.h"
+
+
+
+AttysComm::AttysComm(SOCKET _btsocket)
+{
+	btsocket = _btsocket;
+
+	adcMuxRegister = new int[2];
+	adcMuxRegister[0] = 0;
+	adcMuxRegister[1] = 0;
+	adcGainRegister = new int[2];
+	adcGainRegister[0] = 0;
+	adcGainRegister[1] = 0;
+	adcCurrNegOn = new int[2];
+	adcCurrNegOn[0] = 0;
+	adcCurrNegOn[1] = 0;
+	adcCurrPosOn = new int[2];
+	adcCurrPosOn[0] = 0;
+	adcCurrNegOn[1] = 0;
+
+	ringBuffer = new float*[nMem];
+	for (int i = 0; i < nMem; i++) {
+		ringBuffer[i] = new float[NCHANNELS];
+	}
+
+	data = new long[NCHANNELS];
+	raw = new char[256];
+	sample = new float[NCHANNELS];
+	inbuffer = new char[100000];
+	strcpy(inbuffer, "");
+	doRun = 0;
+	inPtr = 0;
+	outPtr = 0;
+	adc_rate_index = ADC_DEFAULT_RATE;
+	timestamp = 0;
+}
+
+
+AttysComm::~AttysComm() {
+	shutdown(btsocket, SD_BOTH);
+	closesocket(btsocket);
+	doRun = 0;
+}
+
+
+/////////////////////////////////////////////////
+// ringbuffer keeping data for chunk-wise plotting
+float* AttysComm::getSampleFromBuffer() {
+	if (inPtr != outPtr) {
+		float* sample = ringBuffer[outPtr];
+		outPtr++;
+		if (outPtr == nMem) {
+			outPtr = 0;
+		}
+		return sample;
+	} else {
+		// cause segfault, should never happen
+		return nullptr;
+	}
+}
+
+
+
+void AttysComm::sendSyncCommand(const char *message, int waitForOK) {
+	// Put the socket in non-blocking mode:
+	char cr[] = "\n";
+	u_long iMode = 1;
+	_RPT0(0, message);
+	_RPT0(0, ": ");
+	ioctlsocket(btsocket, FIONBIO, &iMode);
+	send(btsocket, message, (int)strlen(message), 0);
+	if (!waitForOK) {
+		_RPT0(0, "not waiting for OK\n");
+		return;
+	}
+	for (int k = 0; k < 10; k++) {
+		send(btsocket, message, (int)strlen(message), 0);
+		send(btsocket, cr, (int)strlen(cr), 0);
+		for (int i = 0; i < 100; i++) {
+			Sleep(1);
+			char linebuffer[8192];
+			int ret = recv(btsocket, linebuffer, 8191, 0);
+			if (ret > 1) {
+				linebuffer[ret] = 0;
+				if (strstr(linebuffer, "OK")) {
+					_RPT0(0, linebuffer);
+					_RPT0(0, " -- OK received\n");
+					return;
+				}
+			}
+		}
+		_RPT0(0, "-- no OK received!\n");
+	}
+}
+
+
+
+void AttysComm::sendSamplingRate() {
+	char tmp[256];
+	sprintf(tmp, "r=%d", adc_rate_index);
+	sendSyncCommand(tmp, 1);
+}
+
+void AttysComm::sendFullscaleAccelRange() {
+	char tmp[256];
+	sprintf(tmp, "t=%d", accel_full_scale_index);
+	sendSyncCommand(tmp, 1);
+}
+
+void AttysComm::sendCurrentMask() {
+	char tmp[256];
+	sprintf(tmp, "c=%d", current_mask);
+	sendSyncCommand(tmp, 1);
+}
+
+void AttysComm::sendBiasCurrent() {
+	char tmp[256];
+	sprintf(tmp, "i=%d", current_index);
+	sendSyncCommand(tmp, 1);
+}
+
+void AttysComm::sendGainMux(int channel, int gain, int mux) {
+	char tmp[256];
+	int v = (mux & 0x0f) | ((gain & 0x0f) << 4);
+	switch (channel) {
+	case 0:
+		sprintf(tmp, "a=%d", v);
+		sendSyncCommand(tmp, 1);
+		break;
+	case 1:
+		sprintf(tmp, "b=%d", v);
+		sendSyncCommand(tmp, 1);
+		break;
+	}
+	adcGainRegister[channel] = gain;
+	adcMuxRegister[channel] = mux;
+}
+
+void AttysComm::sendInit() {
+	char rststr[] = "\n\n\n\n\r";
+	OutputDebugStringW(L"Sending Init\n");
+	send(btsocket, rststr, (int)strlen(rststr), 0);
+	sendSyncCommand("x=0", 1);
+	// switching to base64 encoding
+	sendSyncCommand("d=1", 1);
+	sendSamplingRate();
+	sendFullscaleAccelRange();
+	sendGainMux(0, adc0_gain_index, adc0_mux_index);
+	sendGainMux(1, adc1_gain_index, adc1_mux_index);
+	sendCurrentMask();
+	sendBiasCurrent();
+	sendSyncCommand("x=1\n", 0);
+	OutputDebugStringW(L"Init finished. Waiting for data.\n");
+}
+
+
+void AttysComm::run() {
+	int nTrans = 1;
+
+	sendInit();
+
+	strcpy(inbuffer, "");
+
+	doRun = 1;
+
+	// Keep listening to the InputStream until an exception occurs
+	while (doRun) {
+
+		char linebuffer[1024];
+		int ret = recv(btsocket, linebuffer, 1023, 0);
+		if (ret > 0) {
+			linebuffer[ret] = 0;
+			if (!strstr(linebuffer, "OK")) {
+				strcat(inbuffer, linebuffer);
+			}
+		}
+
+		int hasData = 1;
+		// search for LF (CR is first)
+		char* lf = strchr(inbuffer, 0x0A);
+		if (!lf) {
+			hasData = 0;
+		} else {
+			if ((lf - inbuffer) < 1) {
+				hasData = 0;
+			} else {
+				*(lf - 1) = 0;
+			}
+		}
+
+		if ((hasData)&&(!strstr(inbuffer,"OK"))) {
+
+			// _RPT1(0, "%s\n",inbuffer);
+			Base64decode(raw, inbuffer);
+
+			lf++;
+			int rem = (int)strlen(lf);
+			memmove(inbuffer, lf, rem);
+
+			inbuffer[rem] = 0;
+
+
+			for (int i = 0; i < 2; i++) {
+				long v = (raw[i * 3] & 0xff)
+					| ((raw[i * 3 + 1] & 0xff) << 8)
+					| ((raw[i * 3 + 2] & 0xff) << 16);
+				data[INDEX_Analogue_channel_1 + i] = v;
+			}
+
+			for (int i = 0; i < 6; i++) {
+				long v = (raw[8 + i * 2] & 0xff)
+					| ((raw[8 + i * 2 + 1] & 0xff) << 8);
+				data[i] = v;
+			}
+
+			// check that the timestamp is the expected one
+			int ts = 0;
+			nTrans = 1;
+			ts = raw[7];
+			if ((ts - expectedTimestamp) > 0) {
+				if (correctTimestampDifference) {
+					nTrans = 1 + (ts - expectedTimestamp);
+				}
+				else {
+					correctTimestampDifference = true;
+				}
+			}
+			// update timestamp
+			expectedTimestamp = ++ts;
+
+			// acceleration
+			for (int i = INDEX_Acceleration_X;
+				i <= INDEX_Acceleration_Z; i++) {
+				float norm = 0x8000;
+				sample[i] = ((float)data[i] - norm) / norm *
+					getAccelFullScaleRange();
+			}
+
+			// magnetometer
+			for (int i = INDEX_Magnetic_field_X;
+				i <= INDEX_Magnetic_field_Z; i++) {
+				float norm = 0x8000;
+				sample[i] = ((float)data[i] - norm) / norm *
+					MAG_FULL_SCALE;
+				//Log.d(TAG,"i="+i+","+sample[i]);
+			}
+
+			for (int i = INDEX_Analogue_channel_1;
+				i <= INDEX_Analogue_channel_2; i++) {
+				float norm = 0x800000;
+				sample[i] = ((float)data[i] - norm) / norm *
+					ADC_REF / ADC_GAIN_FACTOR[adcGainRegister[i
+					- INDEX_Analogue_channel_1]];
+			}
+
+			// in case a sample has been lost
+			for (int j = 0; j < nTrans; j++) {
+				for (int k = 0; k < NCHANNELS; k++) {
+					ringBuffer[inPtr][k] = sample[k];
+				}
+				timestamp = timestamp + 1.0 / getSamplingRateInHz();
+				sampleNumber++;
+				inPtr++;
+				if (inPtr == nMem) {
+					inPtr = 0;
+				}
+			}
+		}
+	}
+};
---
+++ AttysComm.h
@@ -0,0 +1,319 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2bth.h>
+#include <BluetoothAPIs.h>
+#include <QThread>
+
+#include "base64.h"
+
+#pragma once
+class AttysComm : public QThread
+{
+public:
+	/////////////////////////////////////////////////
+	// Constructor: takes the bluetooth device as an argument
+	// it then tries to connect to the Attys
+	AttysComm(SOCKET _btsocket);
+
+	~AttysComm();
+
+public:
+
+	static const int NCHANNELS = 8;
+
+	// index numbers of the channels returned in the data array
+	const int INDEX_Acceleration_X = 0;
+	const int INDEX_Acceleration_Y = 1;
+	const int INDEX_Acceleration_Z = 2;
+	const int INDEX_Magnetic_field_X = 3;
+	const int INDEX_Magnetic_field_Y = 4;
+	const int INDEX_Magnetic_field_Z = 5;
+	const int INDEX_Analogue_channel_1 = 6;
+	const int INDEX_Analogue_channel_2 = 7;
+
+	// descriptions the channels in text form
+	const char* const CHANNEL_DESCRIPTION[NCHANNELS] = {
+		"Acceleration X",
+		"Acceleration Y",
+		"Acceleration Z",
+		"Magnetic field X",
+		"Magnetic field Y",
+		"Magnetic field Z",
+		"Analogue channel 1",
+		"Analogue channel 2"
+	};
+
+	// units of the channels
+	const char* const CHANNEL_UNITS[NCHANNELS] = {
+		"m/s^2",
+		"m/s^2",
+		"m/s^2",
+		"T",
+		"T",
+		"T",
+		"V",
+		"V"
+	};
+
+	///////////////////////////////////////////////////////////////////
+	// ADC sampling rate and for the whole system
+	const int ADC_RATE_125HZ = 0;
+	const int ADC_RATE_250HZ = 1;
+	const int ADC_RATE_500Hz = 2;
+	const int ADC_DEFAULT_RATE = ADC_RATE_250HZ;
+	// array of the sampling rates converting the index
+	// to the actual sampling rate
+	const int ADC_SAMPLINGRATE[4] = { 125, 250, 500, 1000 };
+	// the actual sampling rate in terms of the sampling rate index
+	int adc_rate_index;
+
+	void setAdc_samplingrate_index(int idx) {
+		adc_rate_index = idx;
+	}
+
+	// get the sampling rate in Hz (not index number)
+	int getSamplingRateInHz() {
+		return ADC_SAMPLINGRATE[adc_rate_index];
+	}
+
+	int getAdc_samplingrate_index() {
+		return adc_rate_index;
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////
+	// ADC gain
+	// the strange numbering scheme comes from the ADC's numbering
+	// scheme. Index=0 is really a gain factor of 6
+	// On the ATttys we refer to channel 1 and 2 which are 0 and 1 here for
+	// indexing.
+	const int ADC_GAIN_6 = 0;
+	const int ADC_GAIN_1 = 1;
+	const int ADC_GAIN_2 = 2;
+	const int ADC_GAIN_3 = 3;
+	const int ADC_GAIN_4 = 4;
+	const int ADC_GAIN_8 = 5;
+	const int ADC_GAIN_12 = 6;
+	// mapping between index and actual gain
+	const int ADC_GAIN_FACTOR[7] = { 6, 1, 2, 3, 4, 8, 12 };
+	// the voltage reference of the ADC in volts
+	const float ADC_REF = 2.42F;
+
+	float getADCFullScaleRange(int channel) {
+		return ADC_REF / ADC_GAIN_FACTOR[adcGainRegister[channel]];
+	}
+
+	void setAdc1_gain_index(int idx) {
+		adc0_gain_index = idx;
+	}
+
+	void setAdc2_gain_index(int idx) {
+		adc1_gain_index = idx;
+	}
+
+	// initial gain factor is 6 for both channels
+	int adc0_gain_index = 0;
+	int adc1_gain_index = 0;
+
+
+	/////////////////////////////////////////////////////////////////////
+	// Bias currents for resistance measurement
+	// selectable bias current index numbers for the ADC inputs
+	// used to measure resistance
+	const int ADC_CURRENT_6NA = 0;
+	const int ADC_CURRENT_22NA = 1;
+	const int ADC_CURRENT_6UA = 2;
+	const int ADC_CURRENT_22UA = 3;
+	int current_index = 0;
+	int current_mask = 0;
+
+	// sets the bias current which can be switched on
+	void setBiasCurrent(int currIndex) {
+		current_index = currIndex;
+	}
+
+	// gets the bias current as in index
+	int getBiasCurrent() {
+		return current_index;
+	}
+
+	// switches the currents on
+	void enableCurrents(int pos_ch1, int neg_ch1, int pos_ch2) {
+		current_mask = 0;
+		if (pos_ch1) {
+			current_mask = (int)(current_mask | (int)0b00000001);
+		}
+		if (neg_ch1) {
+			current_mask = (int)(current_mask | (int)0b00000010);
+		}
+		if (pos_ch2) {
+			current_mask = (int)(current_mask | (int)0b00000100);
+		}
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////////
+	// selectable different input mux settings
+	// for the ADC channels
+	const int ADC_MUX_NORMAL = 0;
+	const int ADC_MUX_SHORT = 1;
+	const int ADC_MUX_SUPPLY = 3;
+	const int ADC_MUX_TEMPERATURE = 4;
+	const int ADC_MUX_TEST_SIGNAL = 5;
+	const int ADC_MUX_ECG_EINTHOVEN = 6;
+	int adc0_mux_index = ADC_MUX_NORMAL;
+	int adc1_mux_index = ADC_MUX_NORMAL;
+
+	void setAdc0_mux_index(int idx) {
+		adc0_mux_index = idx;
+	}
+
+	void setAdc1_mux_index(int idx) {
+		adc1_mux_index = idx;
+	}
+
+
+	///////////////////////////////////////////////////////////////////////////////
+	// accelerometer
+	const int ACCEL_2G = 0;
+	const int ACCEL_4G = 1;
+	const int ACCEL_8G = 2;
+	const int ACCEL_16G = 3;
+	const float oneG = 9.80665F; // m/s^2
+	const float ACCEL_FULL_SCALE[4] = { 2 * oneG, 4 * oneG, 8 * oneG, 16 * oneG }; // m/s^2
+	int accel_full_scale_index = ACCEL_16G;
+
+	float getAccelFullScaleRange() {
+		return ACCEL_FULL_SCALE[accel_full_scale_index];
+	}
+
+	void setAccel_full_scale_index(int idx) {
+		accel_full_scale_index = idx;
+	}
+
+
+	///////////////////////////////////////////////////
+	// magnetometer
+	//
+	const float MAG_FULL_SCALE = 4800.0E-6F; // TESLA
+
+	float getMagFullScaleRange() {
+		return MAG_FULL_SCALE;
+	}
+
+
+	////////////////////////////////////////////////
+	// timestamp stuff as double
+	// note this might drift in the long run
+	void setTimestamp(double ts) {
+		timestamp = ts;
+	}
+
+	double getTimestamp() {
+		return timestamp;
+	}
+
+
+	////////////////////////////////////////////////
+	// sample counter
+	long sampleNumber = 0;
+
+	long getSampleNumber() {
+		return sampleNumber;
+	}
+
+	void setSampleNumber(long sn) {
+		sampleNumber = sn;
+	}
+
+
+	///////////////////////////////////////////////////////////////////////
+	// message listener
+	// sends error/success messages back
+	// for MessageListener
+	// here are the messages:
+	const int MESSAGE_CONNECTED = 0;
+	const int MESSAGE_ERROR = 1;
+	const int MESSAGE_RETRY = 2;
+	const int MESSAGE_CONFIGURE = 3;
+	const int MESSAGE_STARTED_RECORDING = 4;
+	const int MESSAGE_STOPPED_RECORDING = 5;
+	const int MESSAGE_CONNECTING = 6;
+
+	////////////////////////////////////////////
+	// connection info
+	int hasActiveConnection() {
+		return isConnected;
+	}
+
+	int hasFatalError() {
+		return fatalError;
+	}
+
+
+	/////////////////////////////////////////////////
+	// ringbuffer keeping data for chunk-wise plotting
+	float* getSampleFromBuffer();
+
+	int hasSampleAvilabale() {
+		return (inPtr != outPtr);
+	}
+
+	private:
+	///////////////////////////////////////////////////////
+	// from here it's private
+	SOCKET btsocket;
+	int doRun = 0;
+	// ringbuffer
+	float** ringBuffer;
+	// number of entries in the ringbuffer
+	const int nMem = 1000;
+	int inPtr = 0;
+	int outPtr = 0;
+	int isConnected = 0;
+	int fatalError = 0;
+	int* adcMuxRegister;
+	int* adcGainRegister;
+	int* adcCurrNegOn;
+	int* adcCurrPosOn;
+	int expectedTimestamp = 0;
+	int correctTimestampDifference = 0;
+	double timestamp = 0.0; // in secs
+	int connectionEstablished;
+	long* data;
+	char* raw;
+	float* sample;
+	char* inbuffer;
+
+	void sendSyncCommand(const char *message, int checkOK);
+
+	void sendSamplingRate();
+
+	void sendFullscaleAccelRange();
+
+	void sendCurrentMask();
+
+	void sendBiasCurrent();
+
+	void sendGainMux(int channel, int gain, int mux);
+
+	void setADCGain(int channel, int gain) {
+		sendGainMux(channel, gain, adcMuxRegister[channel]);
+	}
+
+	void setADCMux(int channel, int mux) {
+		sendGainMux(channel, adcGainRegister[channel], mux);
+	}
+
+	void sendInit();
+
+	void run();
+
+	public:
+	/* Call this from the main activity to shutdown the connection */
+	void quit() {
+		doRun = false;
+	}
+};
