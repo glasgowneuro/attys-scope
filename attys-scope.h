a8b56c143edc059e2110914ab327ab5bd9aa092f
Bernd Porr <mail@berndporr.me.uk>
2018-12-31 18:19:17 +0000

    New version number

--- attys-scope.h
+++ attys-scope.h
@@ -29,7 +29,7 @@ class Attys_scope;
 #include "AttysScan.h"
 
 // version number
-#define VERSION "1.5.2"
+#define VERSION "1.5.3"
 
 #define ATTYS_STRING "ATTYS"
 #define PROGRAM_NAME "attys-scope"
