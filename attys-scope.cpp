66a7d72f807a1b86fdc0fe492781159227b09279
Bernd Porr <mail@berndporr.me.uk>
2020-02-16 11:33:02 +0000

    Replaced Sleep with chrono

--- attys-scope.cpp
+++ attys-scope.cpp
@@ -729,7 +729,7 @@ int main( int argc, char **argv )
 	// zero on success and non zero on failure
 	if (ret) {
 		a.processEvents();
-		Sleep(5000);
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 		delete splash;
 		return ret;
 	}
@@ -739,7 +739,7 @@ int main( int argc, char **argv )
 		printf("No Attys present or not paired.\n");
 		splash->showMessage("Cound not connect\nand/or no devices paired.");
 		a.processEvents();
-		Sleep(5000);
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 		delete splash;
 		return -1;
 	}
