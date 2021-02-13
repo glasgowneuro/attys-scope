3d80e0448123e1197513fd141f0ed40085a22bf9
Bernd Porr <mail@berndporr.me.uk>
2021-02-13 00:14:32 +0000

    Updated for the new channel layout

--- python/plot_demo_data.py
+++ python/plot_demo_data.py
@@ -3,22 +3,50 @@
 #
 import numpy as np
 import pylab as pl
+import scipy.signal as signal
 #
-data = np.loadtxt('demodata.tsv');
+data = np.loadtxt('mydata.tsv');
 #
 fig = pl.figure(1)
 #
-pl.title('Demo data measured with the Attys');
-# I
+pl.title('ADC data measured with the Attys');
+
+f1 = 45
+f2 = 55
+fs = 250
+b1,a1 = signal.butter(2, [f1/fs*2,f2/fs*2], 'stop')
+b2,a2 = signal.butter(2, 0.75/fs*2, 'high')
+
+ch1 = data[:,7]
+ch1 = signal.lfilter(b1,a1,ch1)
+ch1 = signal.lfilter(b2,a2,ch1)
 pl.subplot(211);
-pl.plot(data[:,0],data[:,9]);
+pl.plot(data[:,0],ch1);
 pl.xlabel('time/sec');
 pl.ylabel('Ch1/V');
-pl.ylim(-2,2);
-# II
+pl.xlim(2,);
+
+
+ch2 = data[:,8]
+ch2 = signal.lfilter(b1,a1,ch2)
+ch2 = signal.lfilter(b2,a2,ch2)
 pl.subplot(212);
-pl.plot(data[:,0],data[:,10]);
+pl.plot(data[:,0],ch2);
 pl.xlabel('time/sec');
 pl.ylabel('Ch2/V');
-pl.ylim(0,1);
+pl.xlim(2,);
+
+pl.figure(2)
+
+ch1 = ch1[fs*2:]
+ch2 = ch2[fs*2:]
+pl.subplot(211);
+pl.plot(np.linspace(0,250,len(ch1)),np.abs(np.fft.fft(ch1)));
+pl.xlabel('f');
+pl.ylabel('ampl');
+
+pl.subplot(212);
+pl.plot(np.linspace(0,250,len(ch2)),np.abs(np.fft.fft(ch2)));
+pl.xlabel('f');
+pl.ylabel('ampl');
 pl.show();
